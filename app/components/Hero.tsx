9e6503d22acdfdad3ba30dccf7b1ab784522944e
Jai Bhalla <jai.bhalla2021@vitstudent.ac.in>
2025-01-22 16:23:36 +0530

    add animations to hero section

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,46 +1,138 @@
-import React from 'react'
-import hero from '../../public/images/heroImage.png'
-import hero1 from '../../public/images/heroImage1.jpg'
-import Image from 'next/image'
+'use client';
+
+import React from 'react';
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import hero from '../../public/images/heroImage.png';
+import hero1 from '../../public/images/heroImage1.jpg';
 
 function Hero() {
   return (
     <div className="text-white h-screen bg-[#0d0d0d] relative rounded-b-3xl overflow-hidden">
-      <Image
-        src={hero}
-        alt=""
-        width={750}
-        className="absolute left-1/2 -translate-x-1/2 -top-32"
-      />
-      {/* Blurred Gradient 1 */}
-      <div
+      <motion.div
+        initial={{y: 500, opacity: 0, rotate: 45}}
+        animate={{y:0, opacity: 1, rotate: 0}}
+        transition={{
+          duration: 1,
+          ease: 'easeInOut',
+          delay: 1
+        }}
+      >
+        <Image
+          src={hero}
+          alt=""
+          width={750}
+          className="absolute left-1/2 -translate-x-1/2 -top-32"
+        />
+      </motion.div>
+    
+      {/* Animated Blurred Gradient 1 */}
+      <motion.div
+        initial={{ opacity: 1, scale: 1, x: 0 }}
+        animate={{ opacity: 0.7, scaleY: 1.2, x: 0}}
+        transition={{
+          duration: 2,
+          ease: 'easeInOut',
+          repeat: Infinity,
+          repeatType: 'reverse',
+        }}
         className="absolute bottom-10 left-0 w-[500px] h-[200px] bg-gradient-to-r from-[#e4ec6b] to-[#eb2626] blur-3xl rotate-[20deg] rounded-[30rem]"
-      ></div>
+      ></motion.div>
 
-      {/* Blurred Gradient 2 */}
-      <div
+      {/* Animated Blurred Gradient 2 */}
+      <motion.div
+        initial={{ opacity: 1, scale: 1, x: 0 }}
+        animate={{ opacity: 0.7, scaleY: 1.2, x: 0}}
+        transition={{
+          duration: 2,
+          ease: 'easeInOut',
+          repeat: Infinity,
+          repeatType: 'reverse',
+        }}
         className="absolute bottom-10 right-0 w-[500px] h-[300px] bg-gradient-to-r from-[#eb2626] to-[#BABC77] blur-3xl -rotate-[20deg] rounded-[30rem]"
-      ></div>
+      ></motion.div>
 
       <div className="absolute left-0 top-0 px-12 w-full flex justify-between items-center h-full">
-        <div className='flex flex-col gap-10 w-[25rem]'>
-            <p>Through innovative design and strategic thinking we create brands that spark connections, inspire loyalty, and elevate your message.</p>
-            <div className='border border-yellow-200 border-dashed p-1 w-48'>
-                <Image src={hero1} alt='' width={200}/>
-            </div>
-            <button className='w-40 text-md bg-white p-2 text-black mt-12'>
-                LET'S CONTACT
-            </button>
+        <div className="flex flex-col gap-10 w-[25rem]">
+          <motion.p
+            initial={{x: -500, opacity: 0, rotate: -45}}
+            animate={{x:0, opacity: 1, rotate: 0}}
+            transition={{
+              duration: 1,
+              ease: 'easeInOut',
+              delay: 1.2
+            }}
+          >
+            Through innovative design and strategic thinking we create brands that spark connections, inspire loyalty, and elevate your message.
+          </motion.p>
+          <motion.div className="border border-yellow-200 border-dashed p-1 w-48"
+            initial={{x: -500, opacity: 0, rotate: -45}}
+            animate={{x:0, opacity: 1, rotate: 0}}
+            transition={{
+              duration: 1,
+              ease: 'easeInOut',
+              delay: 1.4
+            }}
+          >
+            <Image src={hero1} alt="" width={200} />
+          </motion.div>
+          <motion.button className="w-40 text-md bg-white p-2 text-black mt-12"
+            initial={{x: -500, opacity: 0, rotate: -45}}
+            animate={{x:0, opacity: 1, rotate: 0}}
+            transition={{
+              duration: 1,
+              ease: 'easeInOut',
+              delay: 1.6
+            }}
+          >
+            LET'S CONTACT
+          </motion.button>
         </div>
         <div className="w-[35rem] font-[800] text-[4.5rem] text-left leading-[5rem]">
-          <div className='flex justify-end'>CREATING</div> 
-          <div className='flex justify-end'>BRANDS THAT</div> 
-          <div className='flex justify-end'>CONNECT AND</div> 
-          <div className='flex justify-end'>INSPIRE</div>
+          <motion.div 
+            className="flex justify-end"
+            initial={{x: 500, opacity: 0, rotate: -45}}
+            animate={{x:0, opacity: 1, rotate: 0}}
+            transition={{
+              duration: 1,
+              ease: 'easeInOut',
+              delay: 0.2
+            }}
+            >CREATING</motion.div>
+          <motion.div 
+            className="flex justify-end"
+            initial={{x: 500, opacity: 0, rotate: -45}}
+            animate={{x:0, opacity: 1, rotate: 0}}
+            transition={{
+              duration: 1,
+              ease: 'easeInOut',
+              delay: 0.4
+            }}
+            >BRANDS THAT</motion.div>
+          <motion.div 
+            className="flex justify-end"
+            initial={{x: 500, opacity: 0, rotate: -45}}
+            animate={{x:0, opacity: 1, rotate: 0}}
+            transition={{
+              duration: 1,
+              ease: 'easeInOut',
+              delay: 0.6
+            }}
+            >CONNECT AND</motion.div>
+          <motion.div 
+            className="flex justify-end"
+            initial={{x: 500, opacity: 0, rotate: -45}}
+            animate={{x:0, opacity: 1, rotate: 0}}
+            transition={{
+              duration: 1,
+              ease: 'easeInOut',
+              delay: 0.8
+            }}
+            >INSPIRE</motion.div>
         </div>
       </div>
     </div>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,17 +1,35 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react';
 
 function Navbar() {
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50); // Change when scrolled 50px
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
-    <nav className='text-white px-12 py-5 flex justify-between items-center fixed w-full bg-transparent z-50'>
-        <div className='text-xl font-bold'>ellion</div>
-        <ul className='flex justify-between items-center gap-8 text-md'>
-            <li>HOME</li>
-            <li>ABOUT</li>
-            <li>WORK</li>
-            <li>CONTACT</li>
-        </ul>
+    <nav
+      className={`px-12 py-5 flex justify-between items-center fixed w-full z-50 transition-all duration-300 ${
+        isScrolled ? 'bg-white bg-opacity-[70%] text-black shadow-md' : 'bg-transparent text-white'
+      }`}
+    >
+      <div className="text-xl font-bold">ellion</div>
+      <ul className="flex justify-between items-center gap-8 text-md">
+        <li>HOME</li>
+        <li>ABOUT</li>
+        <li>WORK</li>
+        <li>CONTACT</li>
+      </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
